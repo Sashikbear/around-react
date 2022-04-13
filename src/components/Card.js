cb287ac3452abc486912ad76cd4dd47353bb5375
Sashikbear <sashikgr@gmail.com>
2022-04-13 13:26:54 +0300

    Use currentUser context in Card component to show/hide delete button and active like button

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,15 @@
+import { useContext } from "react";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
 function Card({ onCardClick, card, onConfirmDeleteClick }) {
+  const currentUser = useContext(CurrentUserContext);
+  const isOwn = card.owner._id === currentUser._id;
+  const isLiked = card.likes.some((user) => user._id === currentUser._id);
+  const cardDeleteButtonClassName = `button button_type_delete ${
+    isOwn ? "" : "button_hidden"
+  }`;
+  const cardLikeButtonClassName = `button button_type_like ${
+    isLiked ? "button_active" : ""
+  }`;
   function handleClick() {
     onCardClick(card);
   }
@@ -10,7 +21,7 @@ function Card({ onCardClick, card, onConfirmDeleteClick }) {
         style={{ backgroundImage: `url(${card.link})` }}
       />
       <button
-        className='button button_type_delete'
+        className={cardDeleteButtonClassName}
         type='button'
         aria-label='delete'
         onClick={onConfirmDeleteClick}
@@ -19,7 +30,7 @@ function Card({ onCardClick, card, onConfirmDeleteClick }) {
         <h2 className='card__title'>{card.name}</h2>
         <div className='card__likes'>
           <button
-            className='button button_type_like'
+            className={cardLikeButtonClassName}
             type='button'
             aria-label='like'
           ></button>
